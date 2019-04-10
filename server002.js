d1ed8ccf9f61f1100f274c8a82b3d61b17bdf78f
huangjiaxi <trustfor@sina.cn>
2019-04-10 23:01:26 +0800

    ajax上传文件的处理 和图片预览

--- server002.js
+++ server002.js
@@ -12,7 +12,25 @@ app.get('/',(req,res)=>{
 // xxx 就是上传 input file 对应的 name字段
 app.post('/upload',upload.single('xxx'),(req,res)=>{
     console.log(req.file);
-    res.send('here')
+    // 处理 跨域问题 允许所有
+    res.set('Access-Control-Allow-Origin','*')
+
+    // 允许某个
+    // res.set('Access-Control-Allow-Origin','http://xxx.com')
+
+    res.send(req.file.filename)
+})
+
+app.get('/preview/:key',(req,res)=>{
+    console.log(req.params.key) // 获取 key
+    res.sendFile(`yyy/${req.params.key}`,{
+        root:__dirname,
+        headers:{
+            'Content-Type':'image/jpeg'
+        }
+    },(error)=>{
+        res.status(404).send('file not found')
+    })
 })
 
 app.listen(3000)
