5e74dbac1df121f8b7bfdfefd97f404620efbb4a
huangjiaxi <trustfor@sina.cn>
2019-04-13 17:37:36 +0800

    多文件上传服务器server004.js

--- server004.js
+++ server004.js
@@ -1,3 +1,4 @@
+// 多文件上传
 const express = require('express')
 const cors = require('cors') // 设置跨域的插件
 const multer  = require('multer') // 获取上传文件的插件
@@ -13,10 +14,9 @@ app.get('/',(req,res)=>{
 })
 
 // file 就是上传 input file 对应的 name字段
-app.post('/upload',cors(),upload.single('file'),(req,res)=>{
-    let filename = req.file.filename;
-    let object = {id:filename}
-    res.send(JSON.stringify(object)); // 序列化
+app.post('/upload',cors(),upload.array('files', 12),(req,res)=>{
+    console.log(req.files)
+    res.send( JSON.stringify(req.files.map(file=>file.filename)) )
 })
 
 app.get('/preview/:key',cors(),(req,res)=>{
